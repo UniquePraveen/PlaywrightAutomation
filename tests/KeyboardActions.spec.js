94d694b5cd7a3c170ffb5f11e87c8cea2f6e01af
UniquePraveen <htnpraveen@gmail.com>
2024-12-05 16:50:41 +0530

    KeyBoardFunctions

---
+++ tests/KeyboardActions.spec.js
@@ -0,0 +1,21 @@
+import { table } from "console";
+import { test, expect } from "playwright/test";
+
+test("KeyboardActions", async ({ page }) => {
+  await page.goto("https://gotranscript.com/text-compare");
+  await page.fill(
+    "//textarea[placeholder='Paste one version of the text here.']",
+    "Welcome to Automation!"
+  );
+
+  await page.keyboard.press("Meta+A");
+  await page.keyboard.press("Meta+C");
+
+  await page.keyboard.down("Tab");
+  await page.keyboard.up("Tab");
+
+  await page.keyboard.press("Meta+C");
+
+  await page.waitForTimeout(3000)
+});
+//textarea[@placeholder='Paste one version of the text here.']
